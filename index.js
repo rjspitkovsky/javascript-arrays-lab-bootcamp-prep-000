b819d40b540f20cf3572c213eb14cb7871e13750
Richard Spitkovsky <rjspitkovsky@gmail.com>
2017-11-13 16:33:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens.push(name)
+  return kittens
+}
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
   return kittens 
 }
